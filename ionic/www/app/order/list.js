95be8429cafa8f987508e38f77fb92b4babe435f
Lucas Gabriel <90lucasgabriel@gmail.com>
2016-10-10 20:38:39 -0300

    - Adding ActionSheet on-hold; - Fix: ngClick to on-tap;

--- ionic/www/app/order/list.js
+++ ionic/www/app/order/list.js
@@ -7,19 +7,21 @@
 
 	OrderListController.$inject = [
 		'$scope', '$resource', 
-		'$state', '$ionicLoading',
+		'$state', '$ionicLoading', '$ionicActionSheet', '$mdBottomSheet',
 		'Order'
 	];
 
 	function OrderListController(
 		$scope, $resource,
-		$state, $ionicLoading,
+		$state, $ionicLoading, $ionicActionSheet, $mdBottomSheet,
 		Order
 	){
 		var vm          = this;
 		vm.orders       = [];
 		vm.activate     = activate;
 		vm.query        = query;
+		vm.goDetails    = goDetails;
+		vm.showOptions  = showOptions;
 
 		activate();
 
@@ -49,6 +51,44 @@
 						$scope.$broadcast('scroll.refreshComplete');
 					}
 				);
+		}
+
+		function goDetails(order){
+			$state.go('client.orders.details', {id: order.id});
+		}
+
+		function showOptions(order){
+			var items = [{name:'Delete'}, {name:'teste'}, {name: 'hahaha'}];
+			$mdBottomSheet.show({
+		      template: '<md-bottom-sheet class="md-grid" layout="column">  <div layout="row" layout-align="center center" ng-cloak>    <h4>Since <code>clickOutsideToClose = false</code>, drag down or press ESC to close</h4>  </div>  <div ng-cloak>    <md-list flex layout="row" layout-align="center center">      <md-list-item ng-repeat="item in items">        <div>          <md-button class="md-grid-item-content" ng-click="listItemClick($index)">            <md-icon md-svg-src="{{item.icon}}"></md-icon>            <div class="md-grid-text"> {{ item.name }} </div>          </md-button>        </div>      </md-list-item>    </md-list>  </div></md-bottom-sheet>		      ',
+		      
+		    }).then(function(clickedItem) {
+		      $scope.alert = clickedItem['name'] + ' clicked!';
+		    });
+		/*			
+			var hideSheet = $ionicActionSheet.show({
+			  buttons: [
+			    { text: 'Detalhes' },
+			    { text: 'Ver entrega' }
+			  ],
+			  destructiveText: 'Delete',
+			  titleText: 'Opções',
+			  cancelText: 'Cancelar',
+			  cancel: function() {
+			    // add cancel code..
+			  },
+			  buttonClicked: function(index) {
+			  	switch(index){
+			  		case 0:
+			  			$state.go('client.orders.details', {id: order.id});
+			  			break;
+			  		case 1:
+			  			
+			  			break;
+			  	}
+			  }
+			});
+			*/
 		}
 	};
 })();
